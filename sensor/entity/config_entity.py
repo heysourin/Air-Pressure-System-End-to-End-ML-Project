c9bdec0b505afdf9f5c939603b64d9383bbc7f56
heysourin <sourin07@gmail.com>
2024-05-09 02:21:23 +0530

    updated

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -3,20 +3,39 @@ import os
 from sensor.constant import training_pipeline
 
 
-class TrainingPipelineConfig():
-    def __init__(self, timestamp: datetime.now()):
-        timestamp = timestamp.strftime('%m_%d_%Y_%H_%M_%S')
+class TrainingPipelineConfig:
 
-        self.timestamp = training_pipeline.PIPELINE_NAME
-        self.artifact_dir = os.path.join(
+    def __init__(self, timestamp=datetime.now()) -> None:
+
+        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+
+        self.pipeline_name: str = training_pipeline.PIPELINE_NAME
+
+        self.artifact_dir: str = os.path.join(
             training_pipeline.ARTIFACT_DIR, timestamp)
 
         self.timestamp: str = timestamp
 
 
-class DataIngestionConfig():
-    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+class DataIngestionConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig) -> None:
+        # creating data_ingestion folder, ref: diagram
         self.data_ingestion_dir = os.path.join(
             training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME)
-        self.data_ingestion_dir = os.path.join(
-            training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME)
+
+        # creating data_ingestion folder, ref: diagram
+        self.feature_store_file_path: str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
+        )
+
+        self.training_file_path: str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
+        )
+
+        self.testing_file_path: str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
+        )
+
+        self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+
+        self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
